48e7bc3266fd4ee7b4b3b39da70ccd410f086d71
PaycoinMaster <admin@paycoin.com>
2015-05-14 22:12:33 -0400

    Added getInfo, getLatestBlocks

--- api/blockchain.php
+++ api/blockchain.php
@@ -18,6 +18,31 @@ class blockchain
         return new self;
     }
 
+    static public function getInfo()
+    {
+        $wallet  = new jsonRPCClient(HOTWALLET, true);
+        $getInfo = $wallet->getinfo();
+        return $getInfo;
+    }
+
+    static public function getLatestBlocks($limit)
+    {
+        $findAll = R::findAll('blocks', 'order by id desc limit ?', [$limit]);
+        $i       = 0;
+        foreach ($findAll as $findOne) {
+            $return[$i]['block']            = $findOne->height;
+            $return[$i]['hash']             = $findOne->hash;
+            $return[$i]['time']             = strtotime($findOne->time);
+            $return[$i]['mint']             = $findOne->mint;
+            $return[$i]['flags']            = $findOne->flags;
+            $return[$i]['value']            = $findOne->totalvalue;
+            $return[$i]['transactioncount'] = $findOne->transactioncount;
+            $return[$i]['tx']               = $findOne->tx;
+            $i++;
+        }
+        return $return;
+    }
+
     static public function blockLookup($block)
     {
         $findOne = R::findOne('blocks', 'height = ?', [$block]);
@@ -43,18 +68,18 @@ class blockchain
 
     static public function addressLookup($address, $limit = 10)
     {
-        $i       = 0;
-        $sql     = "
+        $i      = 0;
+        $sql    = "
         (SELECT time,value,'send' as type,txid FROM `vin` WHERE `address` LIKE '" . $address . "')
         UNION
         (SELECT time,value,'receive' as type,txidp as txid FROM `vout` WHERE `address` LIKE '" . $address . "')
-        order by time desc LIMIT ".$limit."";
-        $getAll  = R::getAll($sql);
+        order by time desc LIMIT " . $limit . "";
+        $getAll = R::getAll($sql);
         foreach ($getAll as $findOne) {
-             foreach ($findOne as $key => $value) {
-                 $return['data'][$i][$key] = $value;
-             }
-             $i++;
+            foreach ($findOne as $key => $value) {
+                $return['data'][$i][$key] = $value;
+            }
+            $i++;
         }
         $return['count']   = count($getAll);
         $return['address'] = $address;
---
+++ api/network.php
@@ -0,0 +1,13 @@
+<?php
+class network {
+    static public function version() {
+    //    $sql = "
+    //    SELECT COUNT(*)
+    //    FROM (
+    //      SELECT DISTINCT version
+    //      FROM `peerinfo`
+    //      Group By version
+    //    )";
+    //    //$getAll = R::getAll
+    }
+}
